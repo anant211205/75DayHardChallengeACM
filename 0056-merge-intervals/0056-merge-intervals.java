89c5739350a32bf909d8faf6873c2c0a5e0b2ab0
Anant Kumar <136022303+anant211205@users.noreply.github.com>
2024-01-13 15:44:06 +0530

    Time: 8 ms (90.12%), Space: 46.5 MB (23.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,28 +5,40 @@ class Solution {
         int index = 0 ; 
         
         Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0])) ;
+        
+        //optimal
 
         for(int i = 0 ; i < n ; i++){
-
-            int start = intervals[i][0] ;
-            int end = intervals[i][1] ;
-
-            //skipping the intervals which are alreasy merged
-            if(!result.isEmpty() && end <= result.get(result.size() - 1)[1]) continue ;
-
-            for(int j = i + 1 ; j < n ; j++){
-                
-                if(intervals[j][0] <= end){
-
-                    end = Math.max(end , intervals[j][1]) ;
-
-                }else{
-                    break ;
-                }
+            if(result.isEmpty() || intervals[i][0] > result.get(result.size() - 1)[1]){
+                result.add(intervals[i]); 
+            }else{
+                result.get(result.size() - 1)[1] = Math.max(result.get(result.size() - 1)[1] , intervals[i][1]) ;
             }
-
-            result.add(new int[]{start , end});
         }
+
         return result.toArray(new int[result.size()][]) ;
+        //brute force
+        // for(int i = 0 ; i < n ; i++){
+
+        //     int start = intervals[i][0] ;
+        //     int end = intervals[i][1] ;
+
+        //     //skipping the intervals which are alreasy merged
+        //     if(!result.isEmpty() && end <= result.get(result.size() - 1)[1]) continue ;
+
+        //     for(int j = i + 1 ; j < n ; j++){
+                
+        //         if(intervals[j][0] <= end){
+
+        //             end = Math.max(end , intervals[j][1]) ;
+
+        //         }else{
+        //             break ;
+        //         }
+        //     }
+
+        //     result.add(new int[]{start , end});
+        // }
+        // return result.toArray(new int[result.size()][]) ;
     }
 }
