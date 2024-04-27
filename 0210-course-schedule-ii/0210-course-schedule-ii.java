9c2e9abc082e2d46b1e0f49b65168e28849a0b03
Anant Kumar <136022303+anant211205@users.noreply.github.com>
2024-04-27 14:14:22 +0530

    Time: 4 ms (91.63%), Space: 45.8 MB (49.81%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,6 +1,22 @@
 class Solution {
+    public boolean isCycle = false ;
+    public void topoSortDFS(int node ,ArrayList<Integer>[] adj , Stack<Integer> st , boolean[]vis , boolean[] inRec){
+        vis[node] = true; 
+        inRec[node] = true ;
+        for(int i : adj[node]){
+            if(inRec[i] == true){
+                isCycle = true; 
+                return ;
+            }
+            if(!vis[i]){
+                topoSortDFS(i , adj , st , vis , inRec) ;
+            }
+        }
+        inRec[node] = false ;
+        st.push(node) ;
+    }
 
-    public int[] topoOrder(int n , ArrayList<Integer>[] adj , int[]indegree){
+    public int[] topoOrderBFS(int n , ArrayList<Integer>[] adj , int[]indegree){
         Queue<Integer> que = new LinkedList<>() ; 
         int[] order = new int[n] ;
         for(int i=0;i<n;i++){
@@ -31,20 +47,49 @@ class Solution {
 
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         ArrayList<Integer>[] adj = new ArrayList[numCourses] ;
-        int []indegree = new int[numCourses] ;
-        int[] ans = new int[numCourses] ;
         for(int i=0;i<numCourses ;i++){
             adj[i] = new ArrayList<>() ;
         }
+
+        // bfs
+        int []indegree = new int[numCourses] ;
+        int[] ans = new int[numCourses] ;
+        // for(int[] arr : prerequisites){
+        //     int a = arr[0] ;
+        //     int b = arr[1] ;
+
+        //     //b--->a 
+        //     adj[b].add(a) ;
+        //     indegree[a]++ ;
+        // }
+
+        // return topoOrder(numCourses , adj , indegree) ;
+
+        //dfs
+        
         for(int[] arr : prerequisites){
             int a = arr[0] ;
             int b = arr[1] ;
 
             //b--->a 
             adj[b].add(a) ;
-            indegree[a]++ ;
         }
-
-        return topoOrder(numCourses , adj , indegree) ;
+        Stack<Integer> st = new Stack<>() ;
+        boolean []vis = new boolean[numCourses] ;
+        boolean []inRec = new boolean[numCourses] ;
+        int j = 0 ;
+        isCycle = false ;
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){
+                topoSortDFS(i , adj , st , vis , inRec) ;
+            }
+        }
+        while(!st.isEmpty()){
+            ans[j++] = st.pop() ;
+        }
+        if(isCycle){
+            return new int[0] ;
+        }
+        else return ans ;
     }
 }
