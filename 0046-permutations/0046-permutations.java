b5c13f2a9d1c5a8c01459456baf2f7ca3ebe8755
Anant Kumar <136022303+anant211205@users.noreply.github.com>
2024-05-21 20:19:26 +0530

    Time: 1 ms (94.64%), Space: 44.4 MB (40.83%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,27 +2,50 @@ class Solution {
 
     private List<List<Integer>> result = new ArrayList<>() ;
 
-    public void solve(int[]nums , List<Integer> ls , boolean []used){
-        if(ls.size() == nums.length){
-            result.add(new ArrayList<>(ls)) ;
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void solve2(int[]nums , int idx){
+        if(idx == nums.length){
+            List<Integer> permutation = new ArrayList<>();
+            for (int num : nums) {
+                permutation.add(num);
+            }
+            result.add(permutation);
             return ;
         }
 
-        for(int i=0;i<nums.length;i++){
-            if(!used[i]){
-                ls.add(nums[i]) ;
-                used[i] = true ;
-                solve(nums , ls , used) ;
-                ls.remove(ls.size() - 1) ;
-               used[i] = false ;
-            }
-        }   
+        for(int i=idx;i<nums.length;i++){
+            swap(nums , i , idx) ;
+            solve2(nums , idx + 1); 
+            swap(nums , i , idx) ;
+        }
     }
 
+    // public void solve(int[]nums , List<Integer> ls , boolean []used){
+    //     if(ls.size() == nums.length){
+    //         result.add(new ArrayList<>(ls)) ;
+    //         return ;
+    //     }
+
+    //     for(int i=0;i<nums.length;i++){
+    //         if(!used[i]){
+    //             ls.add(nums[i]) ;
+    //             used[i] = true ;
+    //             solve(nums , ls , used) ;
+    //             ls.remove(ls.size() - 1) ;
+    //            used[i] = false ;
+    //         }
+    //     }   
+    // }
+
     public List<List<Integer>> permute(int[] nums) {
-        List<Integer> ls = new ArrayList<>() ;
-        boolean [] used = new boolean[nums.length]; 
-        solve(nums , ls , used) ;
+        // List<Integer> ls = new ArrayList<>() ;
+        // boolean [] used = new boolean[nums.length]; 
+        solve2(nums , 0) ;
         return result ;
     }
 }
