fdd289f8f614aea9ce2470e16abe8bcdf9fcd04c
Anant Kumar <136022303+anant211205@users.noreply.github.com>
2024-06-11 17:34:12 +0530

    Time: 3 ms (83.54%), Space: 45.8 MB (19.81%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,30 +1,66 @@
 class Solution {
     public int candy(int[] ratings) {
+
+        //approach 2 
+
         int n = ratings.length ;
+        int candy = n ;
+        int i = 1; 
+        while(i < n){
 
-        int[] l2r = new int[n] ;
-        int[] r2l = new int[n] ;
-
-        Arrays.fill(l2r , 1) ;
-        Arrays.fill(r2l , 1) ;
-
-        for(int i=1;i<n;i++){
-            if(ratings[i] > ratings[i-1]){
-                l2r[i] = Math.max(l2r[i] , l2r[i-1] + 1) ;
+            if(ratings[i] == ratings[i - 1]){
+                i++;
+                continue ;
             }
-        }
 
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i] > ratings[i+1]){
-                r2l[i] = Math.max(r2l[i] , r2l[i+1] + 1) ;
+            int peak = 0 ;
+            while(ratings[i] > ratings[i - 1]){
+                peak++ ;
+                candy += peak ;
+                i++ ;
+                if(i == n) return candy ;
             }
+
+            int dip = 0 ;
+
+            while(i < n && ratings[i - 1] > ratings[i]){
+                dip++ ;
+                candy += dip ;
+                i++ ;
+            }
+
+            candy -= Math.min(dip , peak) ;
+
         }
 
-        int result = 0 ;
-        for(int i=0;i<n;i++){
-            result += Math.max(r2l[i] , l2r[i]) ;
-        }
+        return candy ;
 
-        return result ;
+        //approach 1 
+        // int n = ratings.length ;
+
+        // int[] l2r = new int[n] ;
+        // int[] r2l = new int[n] ;
+
+        // Arrays.fill(l2r , 1) ;
+        // Arrays.fill(r2l , 1) ;
+
+        // for(int i=1;i<n;i++){
+        //     if(ratings[i] > ratings[i-1]){
+        //         l2r[i] = Math.max(l2r[i] , l2r[i-1] + 1) ;
+        //     }
+        // }
+
+        // for(int i=n-2;i>=0;i--){
+        //     if(ratings[i] > ratings[i+1]){
+        //         r2l[i] = Math.max(r2l[i] , r2l[i+1] + 1) ;
+        //     }
+        // }
+
+        // int result = 0 ;
+        // for(int i=0;i<n;i++){
+        //     result += Math.max(r2l[i] , l2r[i]) ;
+        // }
+
+        // return result ;
     }
 }
